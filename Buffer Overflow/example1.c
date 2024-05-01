70f035cffb4c00597f8ede5e20c226e630b718bb
naalba <168636088+naalba@users.noreply.github.com>
2024-05-01 14:06:35 +0300

    buffer overflow

--- Buffer Overflow/example1.c
+++ Buffer Overflow/example1.c
@@ -2,7 +2,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	char name[64];
 	printf("Enter your name: ");
-	scanf("%s", name);
+	fgets("%s", name);
 	Sanitize(name);
 	printf("Welcome, %s!", name);
 	return 0;
